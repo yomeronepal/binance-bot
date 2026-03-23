091bae7ca8073ac41b8b481f7f8c80c431c4378b
Rojesh-Outcode <rojesh.prajapati@outcodesoftware.com>
2026-03-23 16:01:02 +0545

    notification fixes

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -373,7 +373,7 @@ const LandingPage = () => {
                 </div>
             </footer>
 
-            <style jsx>{`
+            <style>{`
         @keyframes blob {
           0%, 100% { transform: translate(0, 0) scale(1); }
           33% { transform: translate(30px, -50px) scale(1.1); }
